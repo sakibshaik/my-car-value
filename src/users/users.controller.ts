2b5cea66c36e4c9a8b74fe5707e2e4876b6956e2
sakib shaik <sakib.shaik@ft.com>
2022-08-24 22:37:28 +0100

    save User data to DB using service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    return body;
+    this.usersService.create(body.email, body.password);
   }
 }
