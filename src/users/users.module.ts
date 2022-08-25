0b56a00cef1c43e67e8d697fc51c7c6bbe0a39a4
sakib shaik <sakib.shaik@ft.com>
2022-08-25 22:57:14 +0100

    create Auth service and wire it up to use userservice for signup and signin functionality

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])], // This creates a repository for us
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
 })
 export class UsersModule {}
