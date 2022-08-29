a9eb3fccd7374228743c950431fe1bb2ff989cfc
sakib shaik <sakib.shaik@ft.com>
2022-08-29 22:29:21 +0100

    getting Typescript support for type safety of fake services

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,14 +1,15 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 it('Can create an instance of auth service', async () => {
   // creating fake user Service
 
-  const fakeUsersService = {
-    find: () => Promise.resolve(),
+  const fakeUsersService: Partial<UsersService> = {
+    find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
+      Promise.resolve({ id: 1, email, password } as User),
   };
 
   const module = await Test.createTestingModule({
