588ac4d2718329f78dce53745b11636252c8f9b4
sakib shaik <sakib.shaik@ft.com>
2022-08-29 21:55:03 +0100

    [WIP] setting up interceptor and wiring up with decorator and Di in controller

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -1,10 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import {UsersService} from "../users.service";
 
 export const CurrentUser = createParamDecorator(
   (data: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-
-    return request.session.userId
+    return request.currentUser;
   },
 );
