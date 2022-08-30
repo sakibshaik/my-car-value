675d24a3c356498722925003e83b3955ceea2b09
sakib shaik <sakib.shaik@ft.com>
2022-08-30 23:31:47 +0100

    Reports: Create report Endpoint

---
+++ src/reports/dto/create-report.dto.ts
@@ -0,0 +1,39 @@
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsNumber()
+  @Min(1)
+  @Max(1000000)
+  price: number;
+
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(2022)
+  year: number;
+
+  @IsNumber()
+  @IsLongitude()
+  lng: number;
+
+  @IsNumber()
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dto/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  create(body: CreateReportDto) {
+    const report = this.repo.create(body);
+    return this.repo.save(report);
+  }
+}
